6472581c9fce63c45c0fcbeeb67e3c1f54a7deae
Regan <2433712510@qq.com>
2019-08-10 13:13:37 +0800

    学习缓存原理,整合redis缓存

--- spring-boot-cache/src/main/java/com/swd/springboot/service/DepartmentService.java
+++ spring-boot-cache/src/main/java/com/swd/springboot/service/DepartmentService.java
@@ -5,7 +5,6 @@ import com.swd.springboot.mapper.DepartmentMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.Cache;
 import org.springframework.cache.annotation.CacheConfig;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.stereotype.Service;
 
