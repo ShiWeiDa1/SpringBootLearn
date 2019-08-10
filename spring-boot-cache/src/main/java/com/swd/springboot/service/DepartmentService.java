545f3cc8ca78972224aee55658add22a7de174cc
Regan <2433712510@qq.com>
2019-08-10 13:10:51 +0800

    学习缓存原理,整合redis缓存

--- spring-boot-cache/src/main/java/com/swd/springboot/service/DepartmentService.java
+++ spring-boot-cache/src/main/java/com/swd/springboot/service/DepartmentService.java
@@ -3,8 +3,10 @@ package com.swd.springboot.service;
 import com.swd.springboot.bean.Department;
 import com.swd.springboot.mapper.DepartmentMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.Cache;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.stereotype.Service;
 
 /**
@@ -16,11 +18,29 @@ public class DepartmentService {
     @Autowired
     DepartmentMapper departmentMapper;
 
-
+    /**
+     * 代码方式编写缓存需要注入RedisCacheManager;
+     */
+    @Autowired
+    RedisCacheManager departmentCacheManager;
+    /*
     @Cacheable
     public Department getDepartmentById(Integer id){
         System.out.println("DepartmentService");
         Department deptById = departmentMapper.getDeptById(id);
         return  deptById;
+    }*/
+
+    /**
+     * 编码方式编写缓存,使用缓存管理器得到缓存操作
+     * @param id
+     * @return
+     */
+    public Department getDepartmentById(Integer id){
+        Department deptById = departmentMapper.getDeptById(id);
+        //获取某一个缓存
+        Cache dept = departmentCacheManager.getCache("dept");
+        dept.put("dept:1",deptById);
+        return deptById;
     }
 }
